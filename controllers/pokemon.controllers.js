301b4b093826ce23634591c4dc0def486127254b
JSand89 <javier.esand@gmail.com>
2025-07-29 21:37:27 -0500

    create con manejo de error key duplicate 11000

--- controllers/pokemon.controllers.js
+++ controllers/pokemon.controllers.js
@@ -1,4 +1,4 @@
-import Pokemon from "../models/pokemon.model.js"
+import PokemonModel from "../models/pokemon.model.js"
 
 const hola = async (req,res)=>{
     console.log("hola desde controlador")
@@ -7,12 +7,29 @@ const hola = async (req,res)=>{
 
 const createPokemon = async (req,res)=>{
     try {
-        const pokemon = new Pokemon(req.body)
+        const pokemon = new PokemonModel(req.body)
         await pokemon.save()
+        // code 11000 para nombre repetido
         return res.status(201).json(pokemon)
     } catch (error) {
         console.error(error)
-        return res.status(error.status).json(error)
+        if(error.code == 11000){
+            console.error(error)
+            return res.status(500).json(
+                {message:"Nombre de pokemon ya usado",
+                error:error
+            })
+        }
+        return res.status(error.code).json(error)
     }
 }
-export {hola,createPokemon}
+const getPokemons = async (req,res)=>{
+    try {
+        const pokemons = await PokemonModel.find()
+        return res.status(200).json(pokemons)
+    } catch (error) {
+        console.error(error)
+        return res.status(error.code).json(error) 
+    }
+}
+export {hola,createPokemon,getPokemons}
